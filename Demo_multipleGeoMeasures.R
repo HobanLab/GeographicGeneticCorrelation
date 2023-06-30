5b734f3972bd470ef42e1fb05a875f0ecefe5f16
Austin Koontz <austinkoontz11@gmail.com>
2023-06-30 13:40:37 -0500

    Passing variables/functions to clusters individually, rather than a list; renaming functions

--- Demo_multipleGeoMeasures.R
+++ Demo_multipleGeoMeasures.R
@@ -25,8 +25,7 @@ num_reps <- 5
 # ---- GEOGRAPHIC VARIABLES
 # Specify geographic buffer size in meters (i.e. 1 km)
 buffSize <- 1000    
-# Define projections for points (WGS84) and for calculations (which use
-# units in meters and equal area)
+# Define projections for points (WGS84) and for calculations (which use units in meters and equal area)
 ptProj <- "+proj=longlat +datum=WGS84"
 calcProj <- "+proj=eqearth +datum=WGS84"
 # Read in world countries layer (created as part of the gap analysis workflow)
@@ -34,61 +33,57 @@ calcProj <- "+proj=eqearth +datum=WGS84"
 world_poly_clip <- 
   vect(file.path("~/Documents/GeoGenCorr/QUAC_demo/gis_layers/world_countries_10m/world_countries_10m.shp"))
 # ---- PARALLELIZATION
-# Set up relevant cores and make sure libraries (adegenet + terra) are on cluster
+# Set up relevant cores 
 num_cores <- detectCores() - 8 
-# cl <- makeCluster(num_cores)
-# clusterEvalQ(cl, library("adegenet"))
-# clusterEvalQ(cl, library("terra"))
-# clusterEvalQ(cl, library("parallel"))
-# # Specify vector of functions used for running resampling
-# parFuncList <- c("createBuffers", "compareBuffArea", "getAlleleCategories",
-#                  "calculateCoverage", "exSitu_Resample")
-# # Specify vector of variables used for running resampling
-# parVarList <- c("num_reps","buffSize","ptProj","calcProj","world_poly_clip")
-# # Pass required functions and variables to the cluster
-# clusterExport(cl, varlist = c(parFuncList, parVarList))
+cl <- makeCluster(num_cores)
+# Make sure libraries (adegenet + terra) are on cluster
+clusterEvalQ(cl, library("adegenet"))
+clusterEvalQ(cl, library("terra"))
+clusterEvalQ(cl, library("parallel"))
 
 # ---- READ IN GEOGRAPHIC AND GENETIC DATA ----
 # ---- GEOGRAPHIC
-# Read in wild occurrence points. This CSV has 3 columns: sample name,
-# latitude, and longitude. The sample names (and order) have to match 
-# the sample names/order of the genind object (rownams of the genetic matrix)
-# read in below.
+# Read in wild occurrence points. This CSV has 3 columns: sample name,latitude, and longitude. 
+# The sample names (and order) have to match 
+# the sample names/order of the genind object (rownams of the genetic matrix) read in below.
 wildPoints <- read.csv("~/Documents/GeoGenCorr/QUAC_demo/Quercus_acerifolia.csv", header = TRUE)
 
 # ---- GENETIC
 # Read in genind file: Optimized de novo assembly; R80, min-maf=0, 
 # first SNP/locus, 2 populations (garden and wild), no Kessler individuals.
-# Wild sample names/order must match those in the sample name column 
-# of the CSV (above)
+# Wild sample names/order must match those in the sample name column of the CSV (above)
 genpop.filePath <- 
   "/RAID1/IMLS_GCCO/Analysis/Stacks/denovo_finalAssemblies/QUAC/output/populations_R80_NOMAF_1SNP_2Pops_NoK/"
-QUAC.SNP.DN.R80.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
-# Correct popNames of genind. For this analysis, we only utilize wild samples
-pop(QUAC.SNP.DN.R80.genind) <- 
+QUAC.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
+# Correct popNames of genind. For this analysis, we'll only utilize wild samples (i.e. those in pop "wild")
+pop(QUAC.genind) <- 
   factor(read.table(paste0(genpop.filePath, "QUAC_popmap_GardenWild_NoK"), header=FALSE)[,2])
 
 # ---- RESAMPLING ----
 # Export the coordinate points data.frame and genind object to the cluster
-# clusterExport(cl, varlist = c("wildPoints", "QUAC.SNP.DN.R80.genind"))
+# clusterExport(cl, varlist = c("wildPoints", "QUAC.genind"))
+clusterExport(cl, varlist = c("wildPoints","QUAC.genind","num_reps","buffSize","ptProj","calcProj","world_poly_clip"))
+clusterExport(cl, varlist = c("createBuffers", "compareBuffArea", "getAlleleCategories","calculateCoverage",
+                              "exSituResample", "geo.gen.Resample", "geo.gen.Resample.Parallel"))
 # Specify file path, for saving resampling array
-arrayDir <- paste0(resamplingDataDir, "QUACdemoResampArr.Rdata")
+arrayDir <- paste0(resamplingDataDir, "QUACdemoResampArr2.Rdata")
 # Run resampling
 print("%%% BEGIN RESAMPLING %%%")
-# exSitu_Resample_Parallel(QUAC.SNP.DN.R80.genind, wildPoints, buffSize, 
-#                          ptProj, calcProj, world_poly_clip, num_reps,
-#                          arrayDir, cluster=cl)
 
+# Below line generates error:
+# "Error in evaluating the argument 'x' in selecting a method for function 't': NULL value passed as symbol address"
+QUAC_demoArray_Par <- geo.gen.Resample.Parallel(gen_obj=QUAC.genind, geo_coordPts=wildPoints,
+                                                geo_ptProj=ptProj, geo_buffProj=calcProj,
+                                                geo_boundary=world_poly_clip, reps=5,
+                                                arrayFilepath=arrayDir, cluster=cl)
 
-QUAC_demoArray <- geo.gen.Resample(gen_obj = QUAC.SNP.DN.R80.genind, geo_coordPts = wildPoints,
+# Working: Run resampling (single replicate)
+exSituResample(QUAC.genind, wildPoints, geo_boundary=world_poly_clip)
+# Working: Run resampling (5 replicates, not in parallel), and save array
+QUAC_demoArray <- geo.gen.Resample(gen_obj = QUAC.genind, geo_coordPts = wildPoints,
                                    geo_buff = buffSize, geo_ptProj = ptProj, geo_buffProj = calcProj,
                                    geo_boundary = world_poly_clip, reps = 5)
-# Save resampling array
-saveRDS(QUAC_demoArray, file=arrayDir)
-
-# Run resampling (single replicate)
-exSitu_Resample(QUAC.SNP.DN.R80.genind, wildPoints, buffSize, 
-                ptProj, calcProj, world_poly_clip)
+saveRDS(QUAC_demoArray, file=paste0(resamplingDataDir, "QUACdemoResampArr.Rdata"))
 
 # ---- PLOTTING ----
 # Specify plot colors
@@ -142,4 +137,4 @@ legend(x=70, y=80, inset = 0.05,
        col=plotColors, pch = c(20,20,20), cex=0.9, pt.cex = 2, bty="n", y.intersp = 0.3)
 
 # Close cores
-# stopCluster(cl)
+stopCluster(cl)
