417bebb995ebb737f9a79f84a350887e6c3839ff
Austin Koontz <austinkoontz11@gmail.com>
2023-07-06 15:54:57 -0500

    Cleaning up filepaths, fine tuning some graphs

--- Demo_QUAC.R
+++ Demo_QUAC.R
@@ -15,25 +15,26 @@ library(RColorBrewer)
 library(scales)
 
 # Read in relevant functions
-GeoGenCorr.wd <- "/home/akoontz/Documents/GeoGenCorr/Code/"
+GeoGenCorr.wd <- "/home/akoontz/Documents/GeoGenCorr/"
 setwd(GeoGenCorr.wd)
-source("functions_GeoGenCoverage.R")
+source("Code/functions_GeoGenCoverage.R")
 
 # ---- VARIABLES ----
 # Declare a directory within which to store .Rdata objects of resampling arrays
-resamplingDataDir <- paste0(GeoGenCorr.wd, "resamplingData/")
+resamplingDataDir <- paste0(GeoGenCorr.wd, "Code/resamplingData/")
 # Specify number of resampling replicates. 
 num_reps <- 5
 # ---- GEOGRAPHIC VARIABLES
-# Specify geographic buffer size in meters (i.e. 1 km)
-buffSize <- 1000    
+# Specify geographic buffer size in meters (i.e. 1 km, or 50 km)
+buffSize <- 1000
+# buffSize <- 50000
 # Define projections for points (WGS84) and for calculations (which use units in meters and equal area)
 ptProj <- "+proj=longlat +datum=WGS84"
 calcProj <- "+proj=eqearth +datum=WGS84"
 # Read in world countries layer (created as part of the gap analysis workflow)
 # This layer is used to clip buffers, to make sure they're not in the water
 world_poly_clip <- 
-  vect(file.path("~/Documents/GeoGenCorr/QUAC_demo/gis_layers/world_countries_10m/world_countries_10m.shp"))
+  vect(file.path(paste0(GeoGenCorr.wd, "GIS_shpFiles/world_countries_10m/world_countries_10m.shp")))
 # This shapefile is by default a "non-exportable" object, which means it must be processed before it can be
 # exported to the cluster (for geographic calculations). The terra::wrap function is used to do this.
 world_poly_clip_W <- wrap(world_poly_clip)
@@ -48,33 +49,32 @@ clusterEvalQ(cl, library("terra"))
 clusterEvalQ(cl, library("parallel"))
 
 # ---- READ IN GEOGRAPHIC AND GENETIC DATA ----
+# Specify filepath for QUAC geographic and genetic data
+QUAC.filePath <- paste0(GeoGenCorr.wd, "Datasets/QUAC/")
+
 # ---- GEOGRAPHIC
-# Read in wild occurrence points. This CSV has 3 columns: sample name,latitude, and longitude. 
-# The sample names (and order) have to match 
-# the sample names/order of the genind object (rownams of the genetic matrix) read in below.
-wildPoints <- read.csv("~/Documents/GeoGenCorr/QUAC_demo/Quercus_acerifolia.csv", header=TRUE)
+# Read in wild occurrence points. This CSV has 3 columns: sample name, latitude, and longitude. 
+# The sample names (and order) have to match the sample names/order of the genind object 
+# (rownams of the genetic matrix) read in below.
+wildPoints <- read.csv(paste0(QUAC.filePath, "Quercus_acerifolia.csv"), header=TRUE)
 
 # ---- GENETIC
 # Read in genind file: Optimized de novo assembly; R80, min-maf=0, 
 # first SNP/locus, 2 populations (garden and wild), no Kessler individuals.
 # Wild sample names/order must match those in the sample name column of the CSV (above)
-genpop.filePath <- 
-  "/RAID1/IMLS_GCCO/Analysis/Stacks/denovo_finalAssemblies/QUAC/output/populations_R80_NOMAF_1SNP_2Pops_NoK/"
-QUAC.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
+QUAC.genind <- read.genepop(paste0(QUAC.filePath,"populations.snps.gen"))
 # Correct popNames of genind. For this analysis, we'll only utilize wild samples (i.e. those in pop "wild")
 pop(QUAC.genind) <- 
-  factor(read.table(paste0(genpop.filePath, "QUAC_popmap_GardenWild_NoK"), header=FALSE)[,2])
+  factor(read.table(paste0(QUAC.filePath, "QUAC_popmap_GardenWild_NoK"), header=FALSE)[,2])
 
 # ---- RESAMPLING ----
 # Export the coordinate points data.frame and genind object to the cluster
-# clusterExport(cl, varlist = c("wildPoints", "QUAC.genind"))
 clusterExport(cl, varlist = c("wildPoints","QUAC.genind","num_reps","buffSize","ptProj","calcProj","world_poly_clip_W"))
 clusterExport(cl, varlist = c("createBuffers", "compareBuffArea", "getAlleleCategories","calculateCoverage",
                               "exSituResample", "geo.gen.Resample.Parallel"))
 # Specify file path, for saving resampling array
-arrayDir <- paste0(resamplingDataDir, "QUACdemoResampArr2.Rdata")
+arrayDir <- paste0(QUAC.filePath, "resamplingData/QUAC_1km_5r_resampArr.Rdata")
 # Run resampling
-print("%%% BEGIN RESAMPLING %%%")
 QUAC_demoArray_Par <- geo.gen.Resample.Parallel(gen_obj=QUAC.genind, geo_coordPts=wildPoints,
                                                 geo_ptProj=ptProj, geo_buffProj=calcProj,
                                                 geo_boundary=world_poly_clip_W, reps=5,
@@ -98,50 +98,35 @@ geo_min95SD(QUAC_demoArray_Par)
 averageValueMat <- meanArrayValues(QUAC_demoArray_Par)
 # Generate linear model between both coverage values
 QUAC_model <- lm (Total ~ Geo, data=averageValueMat)
-QUAC_model_summary <- summary(QUAC_model)
+QUAC_model_summary <- summary(QUAC_model) ; QUAC_model_summary
 # Pull R-squared and p-value estimates from model
 QUAC_model_rSquared <- QUAC_model_summary$adj.r.squared
 QUAC_model_pValue <- QUAC_model_summary$coefficients[2, 4]
 
-# PLOTTING ----
+# %%%% GEOGRAPHIC-GENETIC CORRELATION
 plot(averageValueMat$Geo, averageValueMat$Total, pch=20, main="Q. acerifolia: Geographic by genetic coverage",
      xlab="Geographic coverage (%)", ylab="Genetic coverage (%)")
 mtext(text="91 Individuals; 1 km buffer; 5 replicates", side=3, line=0.3)
 mylabel = bquote(italic(R)^2 == .(format(QUAC_model_rSquared, digits = 3)))
-text(x = 35, y = 95, labels = mylabel)
+text(x = 45, y = 95, labels = mylabel)
 
-# %%%% TOTAL ALLELIC AND GEOGRAPHIC COLORS
+# %%%% TOTAL ALLELIC AND GEOGRAPHIC COVERAGE
 # Use the matplot function to plot the matrix of average values, with specified settings
-matplot(averageValueMat, ylim=c(0,100), col=plotColors, pch=16, ylab="Coverage (%)")
+matplot(averageValueMat, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab="Coverage (%)")
 # Add title and x-axis labels to the graph
-title(main="Quercus acerifolia: Geo-Gen Coverage (5 resampling replicates)", line=0.5)
-mtext(text="Number of individuals (91 Total)", side=1, line=2.4)
+title(main="Quercus acerifolia: Geo-Gen Coverage", line=1.5)
+mtext(text="91 Individuals; 1 km buffer; 5 replicates", side=3, line=0.3)
+mtext(text="Number of individuals", side=1, line=2.4)
 # Mark the 95% threshold line, and the genetic/geographic points
 abline(h=95, col="black", lty=3) 
 abline(v=gen_min95Value, col="red")
 abline(v=geo_min95Value, col="darkblue")
 # Add text for the minimum sampling size lines
 mtext(text=paste0("Gen 95% MSSE = ", gen_min95Value),
-      side=1, line=-1.5, at=70, cex=1)
+      side=1, line=-1.5, at=76, cex=1)
 mtext(text=paste0("Geo 95% MSSE = ", geo_min95Value),
-      side=1, line=-1.5, at=15, cex=1)
+      side=1, line=-1.5, at=10, cex=1)
 # Add legend
 legend(x=65, y=80, inset = 0.05,
-       legend = c("Genetic coverage (Total)", "Geographic coverage (1km buffer)"),
-       col=plotColors, pch = c(20,20,20), cex=0.9, pt.cex = 2, bty="n", y.intersp = 0.3)
-
-# # %%%% ALL ALLELE CATEGORIES
-# # Use the matplot function to plot the matrix of average values, with specified settings
-# matplot(averageValueMat, ylim=c(0,100), col=plotColors, pch=16, ylab="Coverage (%)")
-# # Add title and x-axis labels to the graph
-# title(main="Quercus acerifolia: Geographic and Genetic Coverage", line=0.5)
-# mtext(text="Number of individuals", side=1, line=2.4)
-# # Mark the 95% threshold line, as well as the 95% minimum sampling size
-# abline(h=95, col="black", lty=3); abline(v=min95_Value, col="black")
-# # Add text for the minimum sampling size line. Location based on min 95 value and function argument
-# mtext(text=paste0("Minimum sampling size (95%) = ", min95_Value),
-#       side=1, line=-1.5, at=50, cex=1)
-# # Add legend
-# legend(x=70, y=80, inset = 0.05,
-#        legend = c("Total","Very common","Common","Low frequency", "Rare", "Geo. coverage"),
-#        col=plotColors, pch = c(20,20,20), cex=0.9, pt.cex = 2, bty="n", y.intersp = 0.3)
+       legend = c("Genetic coverage (Total)", "Geographic coverage (1 km buffer)"),
+       col=plotColors_Sub, pch = c(20,20,20), cex=0.9, pt.cex = 2, bty="n", y.intersp = 1)
