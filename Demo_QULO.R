8fef7e541cab577b832626f9b92d177461e18e5a
Austin Koontz <austinkoontz11@gmail.com>
2023-08-25 13:23:34 -0500

    Updated plotting for eco coverage calculations

--- Demo_QULO.R
+++ Demo_QULO.R
@@ -90,79 +90,107 @@ QULO_demoArray_Par <-
 # Close cores
 stopCluster(cl)
 
-# ---- CORRELATION AND PLOTTTING ----
-# Specify plot colors
-# plotColors <- c("red","red4","darkorange3","coral","purple", "darkblue")
-# plotColors[2:5] <- alpha(plotColors[2:5], 0.35)
-# plotColors_Sub <- plotColors[-(2:5)]
-# 
-# # Calculate minimum 95% sample size for genetic and geographic values
-# gen_min95Value <- gen_min95Mean(QULO_demoArray_Par) ; gen_min95Value
-# gen_min95SD(QULO_demoArray_Par)
-# geo_min95Value <- geo_min95Mean(QULO_demoArray_Par) ; geo_min95Value
-# geo_min95SD(QULO_demoArray_Par)
-# # Generate the average values (across replicates) for all proportions
-# # This function has default arguments for returning just Total allelic and geographic proportions
-# averageValueMat <- meanArrayValues(QULO_demoArray_Par)
-# # Generate linear model between both coverage values
-# QULO_model <- lm (Total ~ Geo, data=averageValueMat)
-# QULO_model_summary <- summary(QULO_model) ; QULO_model_summary
-# # Pull R-squared and p-value estimates from model
-# QULO_model_rSquared <- QULO_model_summary$adj.r.squared
-# QULO_model_pValue <- QULO_model_summary$coefficients[2, 4]
-# 
-# # %%%% GEOGRAPHIC-GENETIC CORRELATION
-# plot(averageValueMat$Geo, averageValueMat$Total, pch=20, 
-#      main="Q. lobata: Geographic by genetic coverage",xlab="", ylab="")
-# mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
-# mtext(text="Geographic coverage (%)", side=1, line=3, cex=1.6)
-# mtext(text="Genetic coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
-# mylabel = bquote(italic(R)^2 == .(format(QULO_model_rSquared, digits = 3)))
-# text(x = 20, y = 85, labels = mylabel, cex=1.4)
-# 
-# # %%%% TOTAL ALLELIC AND GEOGRAPHIC COVERAGE
-# # Use the matplot function to plot the matrix of average values, with specified settings
-# matplot(averageValueMat, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab="")
-# # Add title and x-axis labels to the graph
-# title(main="Quercus lobata: Geo-Gen Coverage", line=1.5)
-# mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
-# mtext(text="Number of individuals", side=1, line=2.4, cex=1.6)
-# mtext(text="Coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
-# # Mark the 95% threshold line, and the genetic/geographic points
-# abline(h=95, col="black", lty=3) 
-# abline(v=gen_min95Value, col="red")
-# abline(v=geo_min95Value, col="darkblue")
-# # Add text for the minimum sampling size lines
-# mtext(text=paste0("Gen 95% MSSE = ", gen_min95Value),
-#       side=1, line=-1.5, at=95, cex=1.3)
-# mtext(text=paste0("Geo 95% MSSE = ", geo_min95Value),
-#       side=1, line=-1.5, at=200, cex=1.3)
-# # Add legend
-# legend(x=205, y=60, inset = 0.05,
-#        legend = c("Genetic coverage (Total)", "Geographic coverage (50 km buffer)"),
-#        col=plotColors_Sub, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 0.8)
-# 
-# # %%%% BOTH PLOTS (For IMLS NLG subgroup presentation, 2023-08-17) ----
-# par(mfrow=c(2,1))
-# 
-# # %%%% TOTAL ALLELIC AND GEOGRAPHIC COVERAGE
-# # Use the matplot function to plot the matrix of average values, with specified settings
-# matplot(averageValueMat, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab="")
-# # Add title and x-axis labels to the graph
-# title(main="Quercus lobata: Geo-Gen Coverage", line=1.5)
-# mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
-# mtext(text="Number of individuals", side=1, line=2.4, cex=1.6)
-# mtext(text="Coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
-# # Mark the 95% threshold line, and the genetic/geographic points
-# abline(h=95, col="black", lty=3) 
-# # Add legend
-# legend(x=215, y=65, inset = 0.05,
-#        legend = c("Genetic coverage (Total)", "Geographic coverage (50 km buffer)"),
-#        col=plotColors_Sub, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 0.6)
-# 
-# # %%%% GEOGRAPHIC-GENETIC CORRELATION
-# plot(averageValueMat$Geo, averageValueMat$Total, pch=20, main="",xlab="", ylab="")
-# mtext(text="Geographic coverage (%)", side=1, line=3, cex=1.6)
-# mtext(text="Genetic coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
-# mylabel = bquote(italic(R)^2 == .(format(QULO_model_rSquared, digits = 3)))
-# text(x = 20, y = 85, labels = mylabel, cex=1.4)
+# ---- CORRELATION ----
+# ---- CALCULATE 95% MSSE AND AVERAGE VALUES
+# Calculate minimum 95% sample size for genetic and geographic values
+gen_min95Value <- gen_min95Mean(QULO_demoArray_Par) ; gen_min95Value
+gen_min95SD(QULO_demoArray_Par)
+geo_min95Value <- geo_min95Mean(QULO_demoArray_Par) ; geo_min95Value
+geo_min95SD(QULO_demoArray_Par)
+eco_min95Value <- eco_min95Mean(QULO_demoArray_Par) ; eco_min95Value
+eco_min95SD(QULO_demoArray_Par)
+# Generate the average values (across replicates) for all proportions
+# This function has default arguments for returning just Total allelic geographic proportions
+averageValueMat <- meanArrayValues(QULO_demoArray_Par, allValues = TRUE)
+# Subset matrix of all average values to just Total allelic coverage, geographic coverage, and ecological coverage
+averageValueMat_TEG <- averageValueMat[,c(1,6,7)]
+# ---- LINEAR MODELS
+# Generate linear models, using Total allelic coverage as the response variable
+# GEOGRAPHIC COVERAGE AS PREDICTOR VARIABLE
+QULO_geoModel <- lm (Total ~ Geo, data=averageValueMat_TEG)
+QULO_geoModel_summary <- summary(QULO_geoModel) ; QULO_geoModel_summary
+# Pull R-squared and p-value estimates from model
+QULO_geoModel_rSquared <- round(QULO_geoModel_summary$adj.r.squared,2)
+QULO_geoModel_pValue <- QULO_geoModel_summary$coefficients[2, 4]
+# ECOLOGICAL COVERAGE AS PREDICTOR VARIABLE
+QULO_ecoModel <- lm (Total ~ Eco, data=averageValueMat_TEG)
+QULO_ecoModel_summary <- summary(QULO_ecoModel) ; QULO_ecoModel_summary
+# Pull R-squared and p-value estimates from model
+QULO_ecoModel_rSquared <- round(QULO_ecoModel_summary$adj.r.squared, 2)
+QULO_ecoModel_pValue <- QULO_ecoModel_summary$coefficients[2, 4]
+
+# ---- PLOTTING ----
+# Specify plot colors ()
+plotColors <- c("red","red4","darkorange3","coral","purple", "darkblue", "darkgreen")
+plotColors <- alpha(plotColors, 0.45)
+plotColors_Sub <- plotColors[-(2:5)]
+
+# ---- CORRELATION PLOTS
+par(mfrow=c(2,1))
+# ---- GEOGRAPHIC-GENETIC
+plot(averageValueMat_TEG$Geo, averageValueMat_TEG$Total, pch=20, xlim=c(0,100), ylim=c(0,110),
+     main="Q. lobata: Geographic by genetic coverage",xlab="", ylab="")
+mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
+mtext(text="Geographic coverage (%)", side=1, line=3, cex=1.6)
+mtext(text="Genetic coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+mylabel = bquote(italic(R)^2 == .(format(QULO_geoModel_rSquared, digits = 3)))
+text(x = 7, y = 85, labels = mylabel, cex=1.4)
+# ---- ECOLOGICAL-GENETIC
+plot(averageValueMat_TEG$Eco, averageValueMat_TEG$Total, pch=20, xlim=c(0,100), ylim=c(0,110),
+     main="Q. lobata: Ecological by genetic coverage",xlab="", ylab="")
+mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
+mtext(text="Ecological coverage (%)", side=1, line=3, cex=1.6)
+mtext(text="Genetic coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+mylabel = bquote(italic(R)^2 == .(format(QULO_ecoModel_rSquared, digits = 3)))
+text(x = 7, y = 85, labels = mylabel, cex=1.4)
+
+# ---- COVERAGE PLOTS
+# Use the matplot function to plot the matrix of average values, with specified settings
+matplot(averageValueMat_TEG, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab="")
+# Add title and x-axis labels to the graph
+title(main="Quercus lobata: Geo-Eco-Gen Coverage", line=1.5)
+mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
+mtext(text="Number of individuals", side=1, line=2.4, cex=1.6)
+mtext(text="Coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+# Mark the 95% threshold line, and the genetic/geographic points
+abline(h=95, col="black", lty=3)
+abline(v=gen_min95Value, col="red")
+abline(v=geo_min95Value, col="darkblue")
+abline(v=eco_min95Value, col="darkgreen")
+# Add text for the minimum sampling size lines
+mtext(text=paste0("Gen 95% MSSE = ", gen_min95Value),
+      side=1, line=-1.5, at=95, cex=1.3)
+mtext(text=paste0("Geo 95% MSSE = ", geo_min95Value),
+      side=1, line=-1.5, at=200, cex=1.3)
+mtext(text=paste0("Eco 95% MSSE = ", eco_min95Value),
+      side=1, line=-3.5, at=111, cex=1.3)
+# Add legend
+legend(x=205, y=60, inset = 0.05,
+       legend = c("Genetic coverage (Total)", "Geographic coverage", "Ecological coverage"),
+       col=c("red", "darkblue", "darkgreen"), pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", 
+       y.intersp = 0.8)
+
+# ---- BOTH PLOTS (For IMLS NLG subgroup presentation, 2023-08-17) ----
+par(mfrow=c(2,1))
+
+# ---- TOTAL ALLELIC AND GEOGRAPHIC COVERAGE
+# Use the matplot function to plot the matrix of average values, with specified settings
+matplot(averageValueMat, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab="")
+# Add title and x-axis labels to the graph
+title(main="Quercus lobata: Geo-Gen Coverage", line=1.5)
+mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
+mtext(text="Number of individuals", side=1, line=2.4, cex=1.6)
+mtext(text="Coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+# Mark the 95% threshold line, and the genetic/geographic points
+abline(h=95, col="black", lty=3)
+# Add legend
+legend(x=215, y=65, inset = 0.05,
+       legend = c("Genetic coverage (Total)", "Geographic coverage (50 km buffer)"),
+       col=plotColors_Sub, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 0.6)
+
+# ---- GEOGRAPHIC-GENETIC CORRELATION
+plot(averageValueMat$Geo, averageValueMat$Total, pch=20, main="",xlab="", ylab="")
+mtext(text="Geographic coverage (%)", side=1, line=3, cex=1.6)
+mtext(text="Genetic coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+mylabel = bquote(italic(R)^2 == .(format(QULO_model_rSquared, digits = 3)))
+text(x = 20, y = 85, labels = mylabel, cex=1.4)
