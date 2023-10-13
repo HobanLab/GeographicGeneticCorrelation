aada6400792547cb56c3a980db0b29dacac61cdd
Austin Koontz <austinkoontz11@gmail.com>
2023-10-13 16:32:03 -0500

    Standardizing function/variable names

--- Demo_QULO.R
+++ Demo_QULO.R
@@ -14,13 +14,11 @@ library(RColorBrewer)
 library(scales)
 
 # Read in relevant functions
-GeoGenCorr.wd <- "/home/akoontz/Documents/GeoGenCorr/"
-setwd(GeoGenCorr.wd)
-source("Code/functions_GeoGenCoverage.R")
+GeoGenCorr_wd <- '/home/akoontz/Documents/GeoGenCorr/'
+setwd(GeoGenCorr_wd)
+source('Code/functions_GeoGenCoverage.R')
 
 # ---- VARIABLES ----
-# Declare a directory within which to store .Rdata objects of resampling arrays
-resamplingDataDir <- paste0(GeoGenCorr.wd, "Code/resamplingData/")
 # Specify number of resampling replicates. 
 num_reps <- 5
 # ---- BUFFER SIZES
@@ -34,11 +32,11 @@ eco_buffSize <- 50000
 # Read in world countries layer (created as part of the gap analysis workflow)
 # This layer is used to clip buffers, to make sure they're not in the water
 world_poly_clip <- 
-  vect(file.path(paste0(GeoGenCorr.wd, "GIS_shpFiles/world_countries_10m/world_countries_10m.shp")))
+  vect(file.path(paste0(GeoGenCorr_wd, 'GIS_shpFiles/world_countries_10m/world_countries_10m.shp')))
 # Read in the EPA Level IV ecoregion shapefile, which is used for calculating ecological coverage (solely in the U.S.)
 ecoregion_poly <- 
-  vect(file.path(paste0(GeoGenCorr.wd, "GIS_shpFiles/ecoregions_EPA_level4/us_eco_l4.shp")))
-# Shapefiles are by default a "non-exportable" object, which means the must be processed before being
+  vect(file.path(paste0(GeoGenCorr_wd, 'GIS_shpFiles/ecoregions_EPA_level4/us_eco_l4.shp')))
+# Shapefiles are by default a 'non-exportable' object, which means the must be processed before being
 # exported to the cluster (for parallelized calculations). The terra::wrap function is used to do this.
 world_poly_clip_W <- wrap(world_poly_clip)
 ecoregion_poly_W <- wrap(ecoregion_poly)
@@ -48,44 +46,44 @@ ecoregion_poly_W <- wrap(ecoregion_poly)
 num_cores <- detectCores() - 4 
 cl <- makeCluster(num_cores)
 # Make sure libraries (adegenet + terra) are on cluster
-clusterEvalQ(cl, library("adegenet"))
-clusterEvalQ(cl, library("terra"))
-clusterEvalQ(cl, library("parallel"))
+clusterEvalQ(cl, library('adegenet'))
+clusterEvalQ(cl, library('terra'))
+clusterEvalQ(cl, library('parallel'))
 
 # ---- READ IN DATA ----
-# Specify filepath for QUAC geographic and genetic data
-QULO.filePath <- paste0(GeoGenCorr.wd, "Datasets/QULO/")
+# Specify filepath for QULO geographic and genetic data
+QULO_filePath <- paste0(GeoGenCorr_wd, 'Datasets/QULO/')
 
 # ---- COORDINATE POINTS
 # Read in wild occurrence points. This CSV has 3 columns: sample name, latitude, and longitude. 
 # The sample names (and order) have to match the sample names/order of the genind object 
 # (rownams of the genetic matrix) read in below.
-wildPoints <- read.csv(paste0(QULO.filePath, "Quercus_lobata.csv"), header=TRUE)
+wildPoints <- read.csv(paste0(QULO_filePath, 'Geographic/Quercus_lobata.csv'), header=TRUE)
 
 # ---- GENETIC MATRIX
 # Processing input file
-QULO.tab <- read.table(paste0(QULO.filePath, "SNPs80.forR"), header = TRUE)
+QULO_tab <- read.table(paste0(QULO_filePath, 'Genetic/SNPs80.forR'), header = TRUE)
 # Make sample names the row names
-rownames(QULO.tab) <- QULO.tab[,1] ; QULO.tab <- QULO.tab[,-1]
+rownames(QULO_tab) <- QULO_tab[,1] ; QULO_tab <- QULO_tab[,-1]
 # Convert to genind. ncode based off similar practice for other geninds (values of 1, 2, and 3 generate identical results)
-QULO.genind <- df2genind(QULO.tab, ncode = 3, ploidy = 2)
+QULO_genind <- df2genind(QULO_tab, ncode = 3, ploidy = 2)
 
 # ---- RESAMPLING ----
 # Export necessary objects (genind, coordinate points, buffer size variables, polygons) to the cluster
-clusterExport(cl, varlist = c("wildPoints","QULO.genind","num_reps","geo_buffSize", "eco_buffSize",
-                              "world_poly_clip_W", "ecoregion_poly_W"))
+clusterExport(cl, varlist = c('wildPoints','QULO_genind','num_reps','geo_buffSize', 'eco_buffSize',
+                              'world_poly_clip_W', 'ecoregion_poly_W'))
 # Export necessary functions (for calculating geographic and ecological coverage) to the cluster
-clusterExport(cl, varlist = c("createBuffers", "geo_compareBuff", "eco_intersectBuff", "eco_compareBuff",
-                              "getAlleleCategories","calculateCoverage", "exSituResample", 
-                              "geo.gen.Resample.Parallel"))
+clusterExport(cl, varlist = c('createBuffers', 'geo.compareBuff', 'eco.intersectBuff', 'eco.compareBuff',
+                              'getAlleleCategories','calculateCoverage', 'exSituResample', 
+                              'geo.gen.Resample.Parallel'))
 # Specify file path, for saving resampling array
-# arrayDir <- paste0(QUAC.filePath, "resamplingData/QULO_1km_GE_5r_resampArr.Rdata")
-arrayDir <- paste0(QULO.filePath, "resamplingData/QULO_50km_GE_5r_resampArr.Rdata")
+# arrayDir <- paste0(QUAC.filePath, 'resamplingData/QULO_1km_GE_5r_resampArr.Rdata')
+arrayDir <- paste0(QULO_filePath, 'resamplingData/QULO_50km_GE_5r_resampArr.Rdata')
 # Run resampling (in parallel)
 QULO_demoArray_Par <- 
-  geo.gen.Resample.Parallel(gen_obj = QULO.genind, geoFlag = TRUE, coordPts = wildPoints, 
+  geo.gen.Resample.Parallel(gen_obj = QULO_genind, geoFlag = TRUE, coordPts = wildPoints, 
                             geoBuff = geo_buffSize, boundary=world_poly_clip_W, ecoFlag = TRUE, 
-                            ecoBuff = eco_buffSize, ecoRegions = ecoregion_poly_W, ecoLayer = "US", 
+                            ecoBuff = eco_buffSize, ecoRegions = ecoregion_poly_W, ecoLayer = 'US', 
                             reps = num_reps, arrayFilepath = arrayDir, cluster = cl)
 # Close cores
 stopCluster(cl)
@@ -125,7 +123,7 @@ averageValueMat <- meanArrayValues(QULO_demoArray_Par, allValues = TRUE)
 averageValueMat_TEG <- averageValueMat[,c(1,6,7)]
 
 # Specify plot colors ()
-plotColors <- c("red","red4","darkorange3","coral","purple", "darkblue", "darkgreen")
+plotColors <- c('red','red4','darkorange3','coral','purple', 'darkblue', 'darkgreen')
 plotColors <- alpha(plotColors, 0.45)
 plotColors_Sub <- plotColors[-(2:5)]
 
@@ -133,45 +131,45 @@ plotColors_Sub <- plotColors[-(2:5)]
 par(mfrow=c(2,1))
 # ---- GEOGRAPHIC-GENETIC
 plot(averageValueMat_TEG$Geo, averageValueMat_TEG$Total, pch=20, xlim=c(0,100), ylim=c(0,110),
-     main="Q. lobata: Geographic by genetic coverage",xlab="", ylab="")
-mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
-mtext(text="Geographic coverage (%)", side=1, line=3, cex=1.6)
-mtext(text="Genetic coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+     main='Q. lobata: Geographic by genetic coverage',xlab='', ylab='')
+mtext(text='436 Individuals; 50 km buffer; 5 replicates', side=3, line=0.3, cex=1.3)
+mtext(text='Geographic coverage (%)', side=1, line=3, cex=1.6)
+mtext(text='Genetic coverage (%)', side=2, line=2.3, cex=1.6, srt=90)
 mylabel = bquote(italic(R)^2 == .(format(QULO_geoModel_rSquared, digits = 3)))
 text(x = 7, y = 85, labels = mylabel, cex=1.4)
 # ---- ECOLOGICAL-GENETIC
 plot(averageValueMat_TEG$Eco, averageValueMat_TEG$Total, pch=20, xlim=c(0,100), ylim=c(0,110),
-     main="Q. lobata: Ecological by genetic coverage",xlab="", ylab="")
-mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
-mtext(text="Ecological coverage (%)", side=1, line=3, cex=1.6)
-mtext(text="Genetic coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+     main='Q. lobata: Ecological by genetic coverage',xlab='', ylab='')
+mtext(text='436 Individuals; 50 km buffer; 5 replicates', side=3, line=0.3, cex=1.3)
+mtext(text='Ecological coverage (%)', side=1, line=3, cex=1.6)
+mtext(text='Genetic coverage (%)', side=2, line=2.3, cex=1.6, srt=90)
 mylabel = bquote(italic(R)^2 == .(format(QULO_ecoModel_rSquared, digits = 3)))
 text(x = 7, y = 85, labels = mylabel, cex=1.4)
 
 # ---- COVERAGE PLOTS
 # Use the matplot function to plot the matrix of average values, with specified settings
-matplot(averageValueMat_TEG, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab="")
+matplot(averageValueMat_TEG, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab='')
 # Add title and x-axis labels to the graph
-title(main="Quercus lobata: Geo-Eco-Gen Coverage", line=1.5)
-mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
-mtext(text="Number of individuals", side=1, line=2.4, cex=1.6)
-mtext(text="Coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+title(main='Quercus lobata: Geo-Eco-Gen Coverage', line=1.5)
+mtext(text='436 Individuals; 50 km buffer; 5 replicates', side=3, line=0.3, cex=1.3)
+mtext(text='Number of individuals', side=1, line=2.4, cex=1.6)
+mtext(text='Coverage (%)', side=2, line=2.3, cex=1.6, srt=90)
 # Mark the 95% threshold line, and the genetic/geographic points
-abline(h=95, col="black", lty=3)
-abline(v=gen_min95Value, col="red")
-abline(v=geo_min95Value, col="darkblue")
-abline(v=eco_min95Value, col="darkgreen")
+abline(h=95, col='black', lty=3)
+abline(v=gen_min95Value, col='red')
+abline(v=geo_min95Value, col='darkblue')
+abline(v=eco_min95Value, col='darkgreen')
 # Add text for the minimum sampling size lines
-mtext(text=paste0("Gen 95% MSSE = ", gen_min95Value),
+mtext(text=paste0('Gen 95% MSSE = ', gen_min95Value),
       side=1, line=-1.5, at=95, cex=1.3)
-mtext(text=paste0("Geo 95% MSSE = ", geo_min95Value),
+mtext(text=paste0('Geo 95% MSSE = ', geo_min95Value),
       side=1, line=-1.5, at=200, cex=1.3)
-mtext(text=paste0("Eco 95% MSSE = ", eco_min95Value),
+mtext(text=paste0('Eco 95% MSSE = ', eco_min95Value),
       side=1, line=-3.5, at=111, cex=1.3)
 # Add legend
 legend(x=205, y=60, inset = 0.05,
-       legend = c("Genetic coverage (Total)", "Geographic coverage", "Ecological coverage"),
-       col=c("red", "darkblue", "darkgreen"), pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", 
+       legend = c('Genetic coverage (Total)', 'Geographic coverage', 'Ecological coverage'),
+       col=c('red', 'darkblue', 'darkgreen'), pch = c(20,20,20), cex=1.2, pt.cex = 2, bty='n', 
        y.intersp = 0.8)
 
 # ---- BOTH PLOTS (For IMLS NLG subgroup presentation, 2023-08-17) ----
@@ -179,22 +177,22 @@ par(mfrow=c(2,1))
 
 # ---- TOTAL ALLELIC AND GEOGRAPHIC COVERAGE
 # Use the matplot function to plot the matrix of average values, with specified settings
-matplot(averageValueMat, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab="")
+matplot(averageValueMat, ylim=c(0,100), col=plotColors_Sub, pch=16, ylab='')
 # Add title and x-axis labels to the graph
-title(main="Quercus lobata: Geo-Gen Coverage", line=1.5)
-mtext(text="436 Individuals; 50 km buffer; 5 replicates", side=3, line=0.3, cex=1.3)
-mtext(text="Number of individuals", side=1, line=2.4, cex=1.6)
-mtext(text="Coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+title(main='Quercus lobata: Geo-Gen Coverage', line=1.5)
+mtext(text='436 Individuals; 50 km buffer; 5 replicates', side=3, line=0.3, cex=1.3)
+mtext(text='Number of individuals', side=1, line=2.4, cex=1.6)
+mtext(text='Coverage (%)', side=2, line=2.3, cex=1.6, srt=90)
 # Mark the 95% threshold line, and the genetic/geographic points
-abline(h=95, col="black", lty=3)
+abline(h=95, col='black', lty=3)
 # Add legend
 legend(x=215, y=65, inset = 0.05,
-       legend = c("Genetic coverage (Total)", "Geographic coverage (50 km buffer)"),
-       col=plotColors_Sub, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 0.6)
+       legend = c('Genetic coverage (Total)', 'Geographic coverage (50 km buffer)'),
+       col=plotColors_Sub, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty='n', y.intersp = 0.6)
 
 # ---- GEOGRAPHIC-GENETIC CORRELATION
-plot(averageValueMat$Geo, averageValueMat$Total, pch=20, main="",xlab="", ylab="")
-mtext(text="Geographic coverage (%)", side=1, line=3, cex=1.6)
-mtext(text="Genetic coverage (%)", side=2, line=2.3, cex=1.6, srt=90)
+plot(averageValueMat$Geo, averageValueMat$Total, pch=20, main='',xlab='', ylab='')
+mtext(text='Geographic coverage (%)', side=1, line=3, cex=1.6)
+mtext(text='Genetic coverage (%)', side=2, line=2.3, cex=1.6, srt=90)
 mylabel = bquote(italic(R)^2 == .(format(QULO_model_rSquared, digits = 3)))
 text(x = 20, y = 85, labels = mylabel, cex=1.4)
