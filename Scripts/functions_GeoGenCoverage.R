5ac1de4a9ec16a07bf4d0bfa7c19b74ed49a5944
Austin Koontz <austinkoontz11@gmail.com>
2024-08-01 17:40:32 -0500

    Added worldAdmin functions (grab and prep) to the script

--- Scripts/functions_GeoGenCoverage.R
+++ Scripts/functions_GeoGenCoverage.R
@@ -622,6 +622,85 @@ getTotalAlleleFreqProportions <- function(gen.obj){
   return(freqProportions)
 }
 
+#' grabWorldAdmin -- Dan Carver
+#'
+#' @param GeoGenCorr_wd : current working directory define by either variable or getwd()
+#' @param fileExtentsion : choice between shp and gpkg.Prefer gpkg for single file. 
+#' @param overwrite : defaults to false; True will force a redownload of the file.
+#'
+#' @description
+#' Checks to see if the file exists. If true it is loaded. If false it is downloaded.
+#' Allow uses to specify the file extention. .shp or .gpkg are options
+#'
+#' @return terra vect object of the world admin layer
+#' 
+grabWorldAdmin <- function(GeoGenCorr_wd, fileExtentsion, overwrite=FALSE){
+  path <- file.path(paste0(GeoGenCorr_wd,
+                           'GIS_shpFiles/world_countries_10m/world_countries_10m',
+                           fileExtentsion))
+  # test for presence of file. 
+  if(file.exists(path) | overwrite == TRUE){
+    # read in for return 
+    world_poly_clip <- terra::vect(path)
+  }else{
+    # download data from natural earth 
+    download <- rnaturalearth::ne_countries(scale = 10,
+                                            type = "countries",
+                                            returnclass = "sf")
+    # convert to terra vect
+    world_poly_clip <- download |>
+      terra::vect()
+    
+    # export the file. 
+    ## create folder structure 
+    dir <- file.path(paste0(GeoGenCorr_wd,'GIS_shpFiles/world_countries_10m'))
+    if(!dir.exists(dir)){
+      dir.create(dir, recursive = TRUE)
+    }
+    # export the file
+    terra::writeVector(x = world_poly_clip, filename = path)
+  }
+  return(world_poly_clip)
+}
+
+#' prepWorldAdmin -- Dan Carver
+#'
+#' @param worldAdmin : full world admin feature
+#' @param occurranceData : tabular records with lat long of know species occurrances 
+#' 
+#' @description
+#' We buffer a convex hull of the occurrance data for the species to 50km. This is used to 
+#' Select the countries that overlap this area. These countries are filterer out of the larger
+#' admin file and dissolve so there are no internal political boundaries.  
+#' 
+#' @return A geographic limits and disvolved verison of the world admin layer that only includes elements that intersect the occurrences
+#' 
+prepWorldAdmin <- function(world_poly_clip, wildPoints){
+  
+  # buffer the wild points to max expected buffer distance 
+  bbox <- vect(wildPoints, 
+               geom = c("decimalLongitude", "decimalLatitude"),
+               crs = crs(world_poly_clip))|> # setting wgs1984
+    terra::convHull() |>
+    terra::buffer(width = 50000)
+  crs(bbox) <- crs(world_poly_clip)
+  
+  # Unit is meter if x has a longitude/latitude CRS
+  # intersect with the admin feature   
+  uniqueLocs <- terra::intersect(world_poly_clip, bbox) 
+  # pull the iso3 for a filter later on 
+  counties <- unique(uniqueLocs$adm0_a3)
+  # filter world admin to countries on overlap
+  admin <- world_poly_clip |>
+    sf::st_as_sf()|>
+    dplyr::filter(adm0_a3 %in% counties)|>
+    terra::vect()|> 
+    terra::aggregate()
+  
+  # return the simplified object 
+  return(admin)
+}
+
 # ---- SDM GEOGRAPHIC COVERAGE FUNCTIONS ----
 # The functions in this section are associated with the SDM approach to calculated geographic coverage,
 # but are not used in the geographic genetic resampling process. Instead, they are used to process the
