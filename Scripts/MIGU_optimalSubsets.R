6bcf60eb096d9f358240f7de38e5220b0d1a41d8
Austin Koontz <austinkoontz11@gmail.com>
2025-07-08 18:00:23 -0500

    Added lines for generating a resampling array of 10 replicates, to compare the geographically optimized data to

--- Scripts/MIGU_optimalSubsets.R
+++ Scripts/MIGU_optimalSubsets.R
@@ -235,3 +235,98 @@ for(i in 1:length(MIGU_geoCoreSets)){
 # Save matrix of genetic coverages to disk
 write.table(genRateMat, file=paste0(MIGU_filePath,'MIGU_CoreSets_GenRates.csv'), 
             row.names=TRUE, sep=',')
+
+# %%% RESAMPLING FOR RANDOMIZED GENETIC COVERAGES (10 REPLICATES) %%% ----
+# Specify geographic buffer size in meters 
+geo_buffSize <- 1000*(c(2,3,4,5,seq(10,100,5),seq(110,250,10),500,1000,1500,2000))
+
+# ---- PARALLELIZATION
+# Set up relevant cores 
+num_cores <- detectCores() - 4 
+cl <- makeCluster(num_cores)
+# Make sure libraries (adegenet, terra, etc.) are on cluster (but avoid printing output)
+invisible(clusterEvalQ(cl, library('adegenet')))
+invisible(clusterEvalQ(cl, library('terra')))
+invisible(clusterEvalQ(cl, library('parallel')))
+invisible(clusterEvalQ(cl, library('usedist')))
+invisible(clusterEvalQ(cl, library('ape')))
+
+# %%% CONDUCT RESAMPLING ----
+# ---- READ IN DATA ----
+# Specify filepath for MIGU geographic and genetic data
+MIGU_filePath <- paste0(GeoGenCorr_wd, 'Datasets/MIGU/')
+
+# ---- GEOGRAPHIC/ECOLOGICAL DATA FILES
+# The supplement of Vallejo-Martin et al. 2021 includes a PDF of the latitudes and longitudes for each population 
+# (SupplementaryData1). This was first converted to a Google Sheet. In order to convert this to a CSV that had latitudes 
+# and longitudes for each individual, individual names were added to the spreadsheet and matched manually 
+# to the lat/long values for each population. Finally, this CSV was subset to include only the 255 individuals
+# present in the native range of western North America.
+MIGU_coordinates <- 
+  read.csv(file=paste0(MIGU_filePath, 'Geographic/MIGU_coordinates.csv'), header = TRUE)
+# Rename the columns of the geographic coordinates data.frame (because geo.compareBuff function expects certain strings)
+colnames(MIGU_coordinates)[2:3] <- c('decimalLatitude', 'decimalLongitude')
+# Read in raster data, for SDM
+MIGU_sdm <- terra::rast(paste0(MIGU_filePath,'Geographic/MIGU_255inds_rast_Carver.tif'))
+
+# Read in world countries layer (created as part of the gap analysis workflow)
+# This layer is used to clip buffers, to make sure they're not in the water
+world_poly_clip <- 
+  vect(file.path(paste0(GeoGenCorr_wd, 'GIS_shpFiles/world_countries_10m/world_countries_10m.shp')))
+# Perform geographic filter on the admin layer. 
+world_poly_clip <- prepWorldAdmin(world_poly_clip = world_poly_clip, wildPoints = MIGU_coordinates)
+# Read in the EPA Level III ecoregion shapefile, which is used for calculating ecological coverage 
+# (in North America)
+ecoregion_poly <- 
+  vect(file.path(paste0(GeoGenCorr_wd, 'GIS_shpFiles/ecoregions_EPA_level3/NA_CEC_Eco_Level3.shp')))
+# Shapefiles are by default a 'non-exportable' object, which means the must be processed before being
+# exported to the cluster (for parallelized calculations). The terra::wrap function is used to do this.
+world_poly_clip_W <- wrap(world_poly_clip)
+ecoregion_poly_W <- wrap(ecoregion_poly)
+MIGU_sdm_W <- wrap(MIGU_sdm)
+
+# ---- GENETIC MATRIX
+# Read in the VCF file provided in Vallejo-Martin et al. 2021 using vcfR::read.vcfR
+MIGU_vcf <- 
+  read.vcfR(file=paste0(MIGU_filePath, 'Genetic/mgut_all_20180305_gut_filter_75.i50.recode.pruned.plink_20180326.vcf'))
+# Convert the vcf to a genind; the return.alleles TRUE value is suggested in the function's help file
+# This genind file is made up of 474 individuals and 1,498 loci
+MIGU_genind_global <- vcfR2genind(MIGU_vcf, sep = "/", return.alleles = TRUE)
+# REMOVE INTRODUCED POPULATIONS: Subset global genind object to only contain individuals from native range. 
+# The 'drop' argument removes alleles no longer present in the dataset.
+MIGU_genind <- MIGU_genind_global[MIGU_coordinates[,1], drop=TRUE]
+
+# ---- RESAMPLING ----
+# Export necessary objects (genind, coordinate points, buffer size variables, polygons) to the cluster
+clusterExport(cl, varlist = c('MIGU_coordinates','MIGU_genind','num_reps','geo_buffSize','eco_buffSize',
+                              'world_poly_clip_W','ecoregion_poly_W','MIGU_sdm_W'))
+# Export necessary functions (for calculating geographic and ecological coverage) to the cluster
+clusterExport(cl, varlist = c('createBuffers','geo.compareBuff','geo.compareBuffSDM','geo.checkSDMres', 
+                              'eco.intersectBuff','eco.compareBuff','gen.getAlleleCategories', 
+                              'gen.buildDistMat', 'gen.calcGenDistCov', 'eco.totalEcoregionCount',
+                              'calculateCoverage','exSituResample.Par', 'geo.gen.Resample.Par'))
+# Specify file path, for saving resampling array
+GeoOptarrayDir <- paste0(MIGU_filePath, 'resamplingData/MIGU_GeoOpt_G2_10r_resampArr.Rdata')
+
+# Run resampling for geographic optimization comparison
+MIGU_demoArray_Par <- 
+  geo.gen.Resample.Par(genObj = MIGU_genind, geoFlag = TRUE, coordPts = MIGU_coordinates, 
+                       geoBuff = geo_buffSize, SDMrast = MIGU_sdm_W, 
+                       boundary=world_poly_clip_W, ecoFlag = FALSE, reps = num_reps, 
+                       arrayFilepath = GeoOptarrayDir, cluster = cl)
+# Close cores
+stopCluster(cl)
+
+# %%% PROCESS THE RESAMPLING ARRAY ----
+# Read in the resampling array .Rdata object, saved to disk
+MIGU_geoOptArray <- readRDS(GeoOptarrayDir)
+
+# Extract only the rows necessary: 7, 11, 49, 59, 63, and 67 individuals
+MIGU_geoRandSubsets <- MIGU_geoOptArray[c(6,10,48,68,62,66),1,]
+colnames(MIGU_geoRandSubsets) <- paste0("Subset",1:10)
+rownames(MIGU_geoRandSubsets) <- c('07 individuals','11 individuals','49 individuals',
+                                  '59 individuals','63 individuals','67 individuals')
+
+# Save matrix of randomized genetic coverages to disk
+write.table(MIGU_geoRandSubsets, file=paste0(MIGU_filePath,'MIGU_RandomSets_GenRates.csv'), 
+            row.names=TRUE, sep=',')
