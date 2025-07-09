92b95b6c9aabd36198c1c03b934ecbc5548707a6
Austin Koontz <austinkoontz11@gmail.com>
2025-07-08 19:34:13 -0500

    Added code for conducting a Wilcox rank sum test, testing whether the average genetic coverage in the optimized datasets are higher than those in randomized datasetes

--- Scripts/MIGU_optimalSubsets.R
+++ Scripts/MIGU_optimalSubsets.R
@@ -206,14 +206,14 @@ MIGU_250km_geoCoreSets <-
 MIGU_500km_geoCoreSets <- 
   read.csv(file='/home/akoontz/Documents/GeoGenCorr/Datasets/Mimulus_guttatus/Geographic/MIGU_geoCoreSets_500km.csv')
 # Make a list of both geograhpic core sets, to loop through later
-MIGU_geoCoreSets  <- list(MIGU_5km_geoCoreSets, MIGU_10km_geoCoreSets, MIGU_25km_geoCoreSets, 
-                          MIGU_50km_geoCoreSets, MIGU_250km_geoCoreSets, MIGU_500km_geoCoreSets)
+MIGU_geoCoreSets  <- list(MIGU_500km_geoCoreSets, MIGU_250km_geoCoreSets, MIGU_50km_geoCoreSets, 
+                          MIGU_25km_geoCoreSets, MIGU_10km_geoCoreSets, MIGU_5km_geoCoreSets)
 
 # %%% CALCULATE GENETIC COVERAGES ----
 # Build a matrix to store genetic coverage rates (columns different core sets, rows different buffer sizes)
-genRateMat <- matrix(NA, nrow=length(MIGU_geoCoreSets), ncol=length(MIGU_geoCoreSets[[1]]))
-colnames(genRateMat) <- colnames(MIGU_5km_geoCoreSets)
-rownames(genRateMat) <- c('5km','10km','25km','50km','250km','500km')
+MIGU_geoOptSubsets <- matrix(NA, nrow=length(MIGU_geoCoreSets), ncol=length(MIGU_geoCoreSets[[1]]))
+colnames(MIGU_geoOptSubsets) <- paste0("Subset",1:10)
+rownames(MIGU_geoOptSubsets) <- c('7n_500km','11n_250km','49n_50km', '59n_25km','63n_10km','67n_5km')
 # Loop through the geographic core sets, calculating the genetic coverages 
 # for each buffer size
 for(i in 1:length(MIGU_geoCoreSets)){
@@ -227,106 +227,133 @@ for(i in 1:length(MIGU_geoCoreSets)){
     MIGU_genSubset <- MIGU_genind[coreSubset,,drop=TRUE]
     # With the given genind object, calculate the geographic coverages, compared
     # to the complete genind object
-    genRateMat[i,j] <- 
+    MIGU_geoOptSubsets[i,j] <- 
       gen.getAlleleCategories(genMat=MIGU_genind@tab, samp=MIGU_genSubset@tab)[1,3]
   }
 }
 
 # Save matrix of genetic coverages to disk
-write.table(genRateMat, file=paste0(MIGU_filePath,'MIGU_CoreSets_GenRates.csv'), 
+write.table(MIGU_geoOptSubsets, file=paste0(MIGU_filePath,'MIGU_CoreSets_GenRates.csv'), 
             row.names=TRUE, sep=',')
 
 # %%% RESAMPLING FOR RANDOMIZED GENETIC COVERAGES (10 REPLICATES) %%% ----
-# Specify geographic buffer size in meters 
-geo_buffSize <- 1000*(c(2,3,4,5,seq(10,100,5),seq(110,250,10),500,1000,1500,2000))
-
-# ---- PARALLELIZATION
-# Set up relevant cores 
-num_cores <- detectCores() - 4 
-cl <- makeCluster(num_cores)
-# Make sure libraries (adegenet, terra, etc.) are on cluster (but avoid printing output)
-invisible(clusterEvalQ(cl, library('adegenet')))
-invisible(clusterEvalQ(cl, library('terra')))
-invisible(clusterEvalQ(cl, library('parallel')))
-invisible(clusterEvalQ(cl, library('usedist')))
-invisible(clusterEvalQ(cl, library('ape')))
-
-# %%% CONDUCT RESAMPLING ----
-# ---- READ IN DATA ----
-# Specify filepath for MIGU geographic and genetic data
-MIGU_filePath <- paste0(GeoGenCorr_wd, 'Datasets/MIGU/')
-
-# ---- GEOGRAPHIC/ECOLOGICAL DATA FILES
-# The supplement of Vallejo-Martin et al. 2021 includes a PDF of the latitudes and longitudes for each population 
-# (SupplementaryData1). This was first converted to a Google Sheet. In order to convert this to a CSV that had latitudes 
-# and longitudes for each individual, individual names were added to the spreadsheet and matched manually 
-# to the lat/long values for each population. Finally, this CSV was subset to include only the 255 individuals
-# present in the native range of western North America.
-MIGU_coordinates <- 
-  read.csv(file=paste0(MIGU_filePath, 'Geographic/MIGU_coordinates.csv'), header = TRUE)
-# Rename the columns of the geographic coordinates data.frame (because geo.compareBuff function expects certain strings)
-colnames(MIGU_coordinates)[2:3] <- c('decimalLatitude', 'decimalLongitude')
-# Read in raster data, for SDM
-MIGU_sdm <- terra::rast(paste0(MIGU_filePath,'Geographic/MIGU_255inds_rast_Carver.tif'))
-
-# Read in world countries layer (created as part of the gap analysis workflow)
-# This layer is used to clip buffers, to make sure they're not in the water
-world_poly_clip <- 
-  vect(file.path(paste0(GeoGenCorr_wd, 'GIS_shpFiles/world_countries_10m/world_countries_10m.shp')))
-# Perform geographic filter on the admin layer. 
-world_poly_clip <- prepWorldAdmin(world_poly_clip = world_poly_clip, wildPoints = MIGU_coordinates)
-# Read in the EPA Level III ecoregion shapefile, which is used for calculating ecological coverage 
-# (in North America)
-ecoregion_poly <- 
-  vect(file.path(paste0(GeoGenCorr_wd, 'GIS_shpFiles/ecoregions_EPA_level3/NA_CEC_Eco_Level3.shp')))
-# Shapefiles are by default a 'non-exportable' object, which means the must be processed before being
-# exported to the cluster (for parallelized calculations). The terra::wrap function is used to do this.
-world_poly_clip_W <- wrap(world_poly_clip)
-ecoregion_poly_W <- wrap(ecoregion_poly)
-MIGU_sdm_W <- wrap(MIGU_sdm)
-
-# ---- GENETIC MATRIX
-# Read in the VCF file provided in Vallejo-Martin et al. 2021 using vcfR::read.vcfR
-MIGU_vcf <- 
-  read.vcfR(file=paste0(MIGU_filePath, 'Genetic/mgut_all_20180305_gut_filter_75.i50.recode.pruned.plink_20180326.vcf'))
-# Convert the vcf to a genind; the return.alleles TRUE value is suggested in the function's help file
-# This genind file is made up of 474 individuals and 1,498 loci
-MIGU_genind_global <- vcfR2genind(MIGU_vcf, sep = "/", return.alleles = TRUE)
-# REMOVE INTRODUCED POPULATIONS: Subset global genind object to only contain individuals from native range. 
-# The 'drop' argument removes alleles no longer present in the dataset.
-MIGU_genind <- MIGU_genind_global[MIGU_coordinates[,1], drop=TRUE]
-
-# ---- RESAMPLING ----
-# Export necessary objects (genind, coordinate points, buffer size variables, polygons) to the cluster
-clusterExport(cl, varlist = c('MIGU_coordinates','MIGU_genind','num_reps','geo_buffSize','eco_buffSize',
-                              'world_poly_clip_W','ecoregion_poly_W','MIGU_sdm_W'))
-# Export necessary functions (for calculating geographic and ecological coverage) to the cluster
-clusterExport(cl, varlist = c('createBuffers','geo.compareBuff','geo.compareBuffSDM','geo.checkSDMres', 
-                              'eco.intersectBuff','eco.compareBuff','gen.getAlleleCategories', 
-                              'gen.buildDistMat', 'gen.calcGenDistCov', 'eco.totalEcoregionCount',
-                              'calculateCoverage','exSituResample.Par', 'geo.gen.Resample.Par'))
-# Specify file path, for saving resampling array
-GeoOptarrayDir <- paste0(MIGU_filePath, 'resamplingData/MIGU_GeoOpt_G2_10r_resampArr.Rdata')
-
-# Run resampling for geographic optimization comparison
-MIGU_demoArray_Par <- 
-  geo.gen.Resample.Par(genObj = MIGU_genind, geoFlag = TRUE, coordPts = MIGU_coordinates, 
-                       geoBuff = geo_buffSize, SDMrast = MIGU_sdm_W, 
-                       boundary=world_poly_clip_W, ecoFlag = FALSE, reps = num_reps, 
-                       arrayFilepath = GeoOptarrayDir, cluster = cl)
-# Close cores
-stopCluster(cl)
+# Declare variable for whether or not resampling needs to be run
+resamplingFlag <- FALSE
+# If resampling has already occurred, read in the array; otherwise, run
+# the resampling workflow
+if(resamplingFlag==TRUE){
+  # Specify geographic buffer size in meters 
+  geo_buffSize <- 1000*(c(2,3,4,5,seq(10,100,5),seq(110,250,10),500,1000,1500,2000))
+  
+  # ---- PARALLELIZATION
+  # Set up relevant cores 
+  num_cores <- detectCores() - 4 
+  cl <- makeCluster(num_cores)
+  # Make sure libraries (adegenet, terra, etc.) are on cluster (but avoid printing output)
+  invisible(clusterEvalQ(cl, library('adegenet')))
+  invisible(clusterEvalQ(cl, library('terra')))
+  invisible(clusterEvalQ(cl, library('parallel')))
+  invisible(clusterEvalQ(cl, library('usedist')))
+  invisible(clusterEvalQ(cl, library('ape')))
+  
+  # %%% CONDUCT RESAMPLING ----
+  # ---- READ IN DATA ----
+  # Specify filepath for MIGU geographic and genetic data
+  MIGU_filePath <- paste0(GeoGenCorr_wd, 'Datasets/MIGU/')
+  
+  # ---- GEOGRAPHIC/ECOLOGICAL DATA FILES
+  # The supplement of Vallejo-Martin et al. 2021 includes a PDF of the latitudes and longitudes for each population 
+  # (SupplementaryData1). This was first converted to a Google Sheet. In order to convert this to a CSV that had latitudes 
+  # and longitudes for each individual, individual names were added to the spreadsheet and matched manually 
+  # to the lat/long values for each population. Finally, this CSV was subset to include only the 255 individuals
+  # present in the native range of western North America.
+  MIGU_coordinates <- 
+    read.csv(file=paste0(MIGU_filePath, 'Geographic/MIGU_coordinates.csv'), header = TRUE)
+  # Rename the columns of the geographic coordinates data.frame (because geo.compareBuff function expects certain strings)
+  colnames(MIGU_coordinates)[2:3] <- c('decimalLatitude', 'decimalLongitude')
+  # Read in raster data, for SDM
+  MIGU_sdm <- terra::rast(paste0(MIGU_filePath,'Geographic/MIGU_255inds_rast_Carver.tif'))
+  
+  # Read in world countries layer (created as part of the gap analysis workflow)
+  # This layer is used to clip buffers, to make sure they're not in the water
+  world_poly_clip <- 
+    vect(file.path(paste0(GeoGenCorr_wd, 'GIS_shpFiles/world_countries_10m/world_countries_10m.shp')))
+  # Perform geographic filter on the admin layer. 
+  world_poly_clip <- prepWorldAdmin(world_poly_clip = world_poly_clip, wildPoints = MIGU_coordinates)
+  # Read in the EPA Level III ecoregion shapefile, which is used for calculating ecological coverage 
+  # (in North America)
+  ecoregion_poly <- 
+    vect(file.path(paste0(GeoGenCorr_wd, 'GIS_shpFiles/ecoregions_EPA_level3/NA_CEC_Eco_Level3.shp')))
+  # Shapefiles are by default a 'non-exportable' object, which means the must be processed before being
+  # exported to the cluster (for parallelized calculations). The terra::wrap function is used to do this.
+  world_poly_clip_W <- wrap(world_poly_clip)
+  ecoregion_poly_W <- wrap(ecoregion_poly)
+  MIGU_sdm_W <- wrap(MIGU_sdm)
+  
+  # ---- GENETIC MATRIX
+  # Read in the VCF file provided in Vallejo-Martin et al. 2021 using vcfR::read.vcfR
+  MIGU_vcf <- 
+    read.vcfR(file=paste0(MIGU_filePath, 'Genetic/mgut_all_20180305_gut_filter_75.i50.recode.pruned.plink_20180326.vcf'))
+  # Convert the vcf to a genind; the return.alleles TRUE value is suggested in the function's help file
+  # This genind file is made up of 474 individuals and 1,498 loci
+  MIGU_genind_global <- vcfR2genind(MIGU_vcf, sep = "/", return.alleles = TRUE)
+  # REMOVE INTRODUCED POPULATIONS: Subset global genind object to only contain individuals from native range. 
+  # The 'drop' argument removes alleles no longer present in the dataset.
+  MIGU_genind <- MIGU_genind_global[MIGU_coordinates[,1], drop=TRUE]
+  
+  # ---- RESAMPLING ----
+  # Export necessary objects (genind, coordinate points, buffer size variables, polygons) to the cluster
+  clusterExport(cl, varlist = c('MIGU_coordinates','MIGU_genind','num_reps','geo_buffSize','eco_buffSize',
+                                'world_poly_clip_W','ecoregion_poly_W','MIGU_sdm_W'))
+  # Export necessary functions (for calculating geographic and ecological coverage) to the cluster
+  clusterExport(cl, varlist = c('createBuffers','geo.compareBuff','geo.compareBuffSDM','geo.checkSDMres', 
+                                'eco.intersectBuff','eco.compareBuff','gen.getAlleleCategories', 
+                                'gen.buildDistMat', 'gen.calcGenDistCov', 'eco.totalEcoregionCount',
+                                'calculateCoverage','exSituResample.Par', 'geo.gen.Resample.Par'))
+  # Specify file path, for saving resampling array
+  GeoOptarrayDir <- paste0(MIGU_filePath, 'resamplingData/MIGU_GeoOpt_G2_10r_resampArr.Rdata')
+  
+  # Run resampling for geographic optimization comparison
+  MIGU_demoArray_Par <- 
+    geo.gen.Resample.Par(genObj = MIGU_genind, geoFlag = TRUE, coordPts = MIGU_coordinates, 
+                         geoBuff = geo_buffSize, SDMrast = MIGU_sdm_W, 
+                         boundary=world_poly_clip_W, ecoFlag = FALSE, reps = num_reps, 
+                         arrayFilepath = GeoOptarrayDir, cluster = cl)
+  # Close cores
+  stopCluster(cl)
+} else {
+  # Specify file path, for saving resampling array
+  GeoOptarrayDir <- paste0(MIGU_filePath, 'resamplingData/MIGU_GeoOpt_G2_10r_resampArr.Rdata')
+  # Read in the existin resampling array .Rdata object, saved to disk
+  MIGU_geoOptArray <- readRDS(GeoOptarrayDir)
+}
 
 # %%% PROCESS THE RESAMPLING ARRAY ----
-# Read in the resampling array .Rdata object, saved to disk
-MIGU_geoOptArray <- readRDS(GeoOptarrayDir)
-
 # Extract only the rows necessary: 7, 11, 49, 59, 63, and 67 individuals
 MIGU_geoRandSubsets <- MIGU_geoOptArray[c(6,10,48,68,62,66),1,]
 colnames(MIGU_geoRandSubsets) <- paste0("Subset",1:10)
-rownames(MIGU_geoRandSubsets) <- c('07 individuals','11 individuals','49 individuals',
-                                  '59 individuals','63 individuals','67 individuals')
+rownames(MIGU_geoRandSubsets) <- c('7n_500km','11n_250km','49n_50km', '59n_25km','63n_10km','67n_5km')
 
 # Save matrix of randomized genetic coverages to disk
 write.table(MIGU_geoRandSubsets, file=paste0(MIGU_filePath,'MIGU_RandomSets_GenRates.csv'), 
             row.names=TRUE, sep=',')
+
+# %%% COMPARE OPTIMIZED AND RANDOMIZED COVERAGES %%% ----
+# Generate a matrix for storing results of Wilcox comparisons
+WilcoxResultsMat <- matrix(nrow=nrow(MIGU_geoOptSubsets), ncol=2)
+rownames(WilcoxResultsMat) <- rownames(MIGU_geoOptSubsets)
+colnames(WilcoxResultsMat) <- c('Statistic', 'P_value')
+# Loop through the rows (sample sizes) of the optimal coverage values
+for(i in 1:nrow(MIGU_geoOptSubsets)){
+  # Conduct the Wilcox test. Here, we're explicitly testing whether the
+  # geographically optimized coverage values (X) are greater than the 
+  # randomized coverage values (Y).
+  result <- 
+    wilcox.test(MIGU_geoOptSubsets[i,], MIGU_geoRandSubsets[i,], alternative = "greater")
+  # The warning here "cannot compute exact p-value with ties" is because 
+  # there are some values which match between the two datasets, which leads
+  # to a normal approximation; this should be safe for our data.
+  WilcoxResultsMat[i,1] <- result$statistic
+  WilcoxResultsMat[i,2] <- result$p.value
+}
+print(WilcoxResultsMat)
